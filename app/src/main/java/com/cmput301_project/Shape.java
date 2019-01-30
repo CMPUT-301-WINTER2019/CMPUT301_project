c061ad84cb600d0acb2022aafa78111031bae16f
Deric Cheung <dacheung@ualberta.ca>
2019-01-29 19:51:06 -0700

    Add z to Shape

--- app/src/main/java/com/cmput301_project/Shape.java
+++ app/src/main/java/com/cmput301_project/Shape.java
@@ -3,11 +3,38 @@ package com.cmput301_project;
 public abstract class Shape {
     private int x;
     private int y;
+    private int z;
+    private String colour;
 
-    Shape() {}
+    public Shape() {}
 
-    Shape(int x, int y) {
+    public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
     }
 }
