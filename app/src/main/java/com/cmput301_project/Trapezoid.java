f56887c54ec4d579c2c02b3975cdd1ade79d61cf
dhoeppne <dhoeppne@ualberta.ca>
2019-01-29 18:40:05 -0700

    Creating trapezoid class

--- app/src/main/java/com/cmput301_project/Trapezoid.java
+++ app/src/main/java/com/cmput301_project/Trapezoid.java
@@ -1,4 +1,11 @@
 package com.cmput301_project;
 
 public class Trapezoid extends Shape {
+    Trapezoid() {
+        super();
+    }
+
+    Trapezoid(Integer x, Integer y) {
+        super(x, y);
+    }
 }
